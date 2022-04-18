0eea66a38e28fdef5c14829c4c125ccb020e40d3
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-04-18 16:45:21 -0400

    Navbar functionality completed

--- navbar.js
+++ navbar.js
@@ -1,11 +1,25 @@
-function navbar(currentPage, target){
-    categoryDropdown(currentPage);
+async function navbar(currentPage, target){
+    let dropdown = await categoryDropdown(currentPage);
     generatedNavbar = `<div class="navbar">
         <img src="images/Logo.png"/>
-        <button>Home</button>
-        <button>About</button>
+        <a href="./index.html">Home</a>
+        ${dropdown}
+        <a href="./about.html">About</a>
     </div>`
+    document.getElementById(target).innerHTML = generatedNavbar;
+    document.getElementById("navbar-dropdown").addEventListener("change", navigate)
 }
-function categoryDropdown(currentPage){
-    fetch('./data.json').then(res => res.json()).then(data => console.log(data));
+async function categoryDropdown(currentPage){
+    let categories = await fetch('./data.json').then(res => res.json()).then(data => Object.keys(data));
+    // if (currentPage in categories)
+    //     categories = [currentPage, ...currentPage];
+    return `<select class="navbar-item" id="navbar-dropdown">
+        ${!categories.includes(currentPage) ? "<option selected disable hidden>Categories</option>":""}
+        ${categories.map(category => `<option ${category == currentPage ? "selected disabled hidden" : ""}>
+            <a href=./category?name=${category}>${category}</a>
+        </option>`).join("")}
+        </select>`
+}
+function navigate(ev){
+    window.location = `./category?name=${ev.target.value}`;
 }
