196bf0bbfbfee36632d9a639ed403a81dffdd370
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-04-18 21:01:04 -0400

    Started adding styling

--- navbar.js
+++ navbar.js
@@ -2,17 +2,15 @@ async function navbar(currentPage, target){
     let dropdown = await categoryDropdown(currentPage);
     generatedNavbar = `<div class="navbar">
         <img src="images/Logo.png"/>
-        <a href="./index.html">Home</a>
+        <a class="navbar-item" href="./index.html">Home</a>
         ${dropdown}
-        <a href="./about.html">About</a>
+        <a class="navbar-item" href="./about.html">About</a>
     </div>`
     document.getElementById(target).innerHTML = generatedNavbar;
     document.getElementById("navbar-dropdown").addEventListener("change", navigate)
 }
 async function categoryDropdown(currentPage){
     let categories = await fetch('./data.json').then(res => res.json()).then(data => Object.keys(data));
-    // if (currentPage in categories)
-    //     categories = [currentPage, ...currentPage];
     return `<select class="navbar-item" id="navbar-dropdown">
         ${!categories.includes(currentPage) ? "<option selected disable hidden>Categories</option>":""}
         ${categories.map(category => `<option ${category == currentPage ? "selected disabled hidden" : ""}>
