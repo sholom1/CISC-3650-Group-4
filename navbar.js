e76bab4d764397eb9e92b7cf1af6f38eb5b5f045
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-04-24 18:51:36 -0400

    Tweaked active class

--- navbar.js
+++ navbar.js
@@ -2,9 +2,9 @@ async function navbar(currentPage, target){
     let dropdown = await categoryDropdown(currentPage);
     let generatedNavbar = `<div class="navbar">
         <img class="navbar-item" src="./Logo.png"/>
-        <a class="navbar-item" href="./index.html">Home</a>
+        <a class="navbar-item ${currentPage === "Home" ? "active": ""}" href="./index.html">Home</a>
         ${dropdown}
-        <a class="navbar-item" href="./about.html">About</a>
+        <a class="navbar-item ${currentPage === "About" ? "active": ""}" href="./about.html">About</a>
     </div>`
     document.getElementById(target).innerHTML = generatedNavbar;
     document.getElementById("navbar-dropdown").addEventListener("change", navigate)
@@ -13,7 +13,7 @@ async function categoryDropdown(currentPage){
     let categories = await fetch('./data.json').then(res => res.json()).then(data => Object.keys(data));
     return `<select class="navbar-item" id="navbar-dropdown">
         ${!categories.includes(currentPage) ? "<option selected disable hidden>Categories</option>":""}
-        ${categories.map(category => `<option ${category == currentPage ? "selected disabled hidden" : ""}>
+        ${categories.map(category => `<option ${category == currentPage ? "selected disabled hidden active" : ""}>
             ${category}
         </option>`).join("")}
         </select>`
